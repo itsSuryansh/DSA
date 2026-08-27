0cc2e5975549503cf1bf65b50f7e53e54a6551c4
Suryansh <reachoutsuryansh@gmail.com>
2026-08-27 21:01:00 +0530

    Time: 0 ms (100%), Space: 71.5 MB (98.6%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int curr = nums[0];
+        int ans = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            curr = max(nums[i], curr + nums[i]);
+            ans = max(ans, curr);
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
