e04af06e6aa9c2ceea9e226fb1f7a16a180cf17c
Suryansh <reachoutsuryansh@gmail.com>
2026-09-02 17:39:04 +0530

    Time: 0 ms (100%), Space: 7.7 MB (58.7%) - LeetHub - Time: O(log columnNumber), Space: O(log columnNumber)

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string ans;
+        while (columnNumber > 0) {
+            columnNumber--; 
+            ans += char('A' + columnNumber % 26);
+            columnNumber /= 26;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
