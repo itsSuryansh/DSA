6c97140269ab524541af9b8bdd5701d034822530
Suryansh <reachoutsuryansh@gmail.com>
2026-09-08 18:41:47 +0530

    Time: 0 ms (100%), Space: 8.4 MB (84.49%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        return max(0, n - 999);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
