2c4a78e147153e58fd4c06acb8677600d5341ba4
Suryansh <reachoutsuryansh@gmail.com>
2026-09-10 18:16:04 +0530

    Time: 0 ms (100%), Space: 8.4 MB (86.14%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mirrorDistance(int n){
+        int original = n;
+        int reversed = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            reversed = reversed * 10 + digit;
+            n /= 10;
+        }
+        return abs(original - reversed);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
